ab0c6d7c14a4eefef040d09eb255e2a1838d9465
b-galazka <bartlomiejgalazka@zoho.eu>
2019-04-28 00:05:42 +0200

    add task form submit and validation

--- src/app/modules/tasks/components/task-form/task-form.component.spec.ts
+++ src/app/modules/tasks/components/task-form/task-form.component.spec.ts
@@ -1,29 +1,175 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TaskFormComponent } from './task-form.component';
-import { ChangeDetectionStrategy } from '@angular/core';
+
+import {
+  FormGroup,
+  ReactiveFormsModule,
+  FormsModule,
+  FormControl,
+  Validators
+} from '@angular/forms';
+
+import { TaskStatus } from 'src/app/core/models/task.model';
+import { taskMock } from 'src/mocks/data/task.mock';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { TextInputComponent } from 'src/app/shared/components/text-input/text-input.component';
+import { TextareaComponent } from 'src/app/shared/components/textarea/textarea.component';
+import { SelectComponent } from 'src/app/shared/components/select/select.component';
+
 
 describe('TaskFormComponent', () => {
   let component: TaskFormComponent;
   let fixture: ComponentFixture<TaskFormComponent>;
+  let debugElement: DebugElement;
+  let formControls: Array<FormControl>;
+
+  function initValues(): void {
+    fixture = TestBed.createComponent(TaskFormComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TaskFormComponent ]
-    })
-    .overrideComponent(TaskFormComponent, {
-      set: { changeDetection: ChangeDetectionStrategy.Default }
+      declarations: [TaskFormComponent, TextInputComponent, TextareaComponent, SelectComponent],
+      imports: [ReactiveFormsModule, FormsModule]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TaskFormComponent);
-    component = fixture.componentInstance;
+    initValues();
     fixture.detectChanges();
+    formControls = <Array<FormControl>> Object.values(component.form.controls);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize form', () => {
+    expect(component.form instanceof FormGroup).toBe(true);
+  });
+
+  it('should submit form on button click', () => {
+
+    component.form.setValue({ name: 'X', description: 'X', status: 'new' });
+
+    const spy = spyOn(component.submit, 'emit');
+
+    const buttonElem: HTMLButtonElement = debugElement
+      .query(By.css('.submit-button')).nativeElement;
+
+    buttonElem.click();
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(component.form.value);
+  });
+
+  it('should mark each field as touched on submit', () => {
+
+    const spies = formControls.map(control => spyOn(control, 'markAsTouched'));
+
+    const buttonElem: HTMLButtonElement = debugElement
+      .query(By.css('.submit-button')).nativeElement;
+
+    buttonElem.click();
+
+    spies.forEach(spy => expect(spy).toHaveBeenCalled());
+  });
+
+  it('should validate each field on submit', () => {
+
+    const spies = formControls.map(control => spyOn(control, 'updateValueAndValidity'));
+
+    const buttonElem: HTMLButtonElement = debugElement
+      .query(By.css('.submit-button')).nativeElement;
+
+    buttonElem.click();
+
+    spies.forEach(spy => expect(spy).toHaveBeenCalled());
+  });
+
+  it('should set each field as required', () => {
+
+    component.form.reset();
+    formControls.forEach(control => control.updateValueAndValidity());
+
+    formControls.forEach(control => expect(control.hasError('required')).toBe(true));
+  });
+
+  it('should set max length validator to 120 characters for name field', () => {
+
+    const nameFormControl = component.form.get('name');
+
+    nameFormControl.setValue('X'.repeat(121));
+    nameFormControl.updateValueAndValidity();
+
+    expect(nameFormControl.getError('maxlength')).toEqual({
+      requiredLength: 120,
+      actualLength: 121
+    });
+  });
+
+  it('should not submit form on button click if form is invalid', () => {
+
+    const spy = spyOn(component.submit, 'emit');
+
+    const buttonElem: HTMLButtonElement = debugElement
+      .query(By.css('.submit-button')).nativeElement;
+
+    component.form.reset();
+    component.form.get('name').setValidators(Validators.required);
+
+    buttonElem.click();
+
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('should disable submit button if form is pending', () => {
+
+    initValues();
+    component.pending = true;
+    fixture.detectChanges();
+
+    const buttonElem: HTMLButtonElement = debugElement
+      .query(By.css('.submit-button')).nativeElement;
+
+    expect(buttonElem.disabled).toBe(true);
+  });
+
+  describe('Creation mode', () => {
+    it('should initialize form with initial values', () => {
+      expect(component.form.value).toEqual({
+        name: '',
+        description: '',
+        status: TaskStatus.New
+      });
+    });
+  });
+
+  describe('Edition mode', () => {
+    beforeEach(() => {
+      initValues();
+      component.existingTask = taskMock;
+      fixture.detectChanges();
+    });
+
+    it('should initialize form with values of provided task', () => {
+      expect(component.form.value).toEqual({
+        name: taskMock.name,
+        description: taskMock.description,
+        status: taskMock.status
+      });
+    });
+
+    it('should disable submit button if form is pristine', () => {
+
+      const buttonElem: HTMLButtonElement = debugElement
+        .query(By.css('.submit-button')).nativeElement;
+
+      expect(buttonElem.disabled).toBe(true);
+    });
   });
 });
--- src/app/modules/tasks/components/task-form/task-form.component.ts
+++ src/app/modules/tasks/components/task-form/task-form.component.ts
@@ -1,4 +1,14 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TaskStatus, ITask, ITaskCreationData } from 'src/app/core/models/task.model';
 
 @Component({
   selector: 'app-task-form',
@@ -6,6 +16,42 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./task-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TaskFormComponent {
+export class TaskFormComponent implements OnInit {
+  @Input() public existingTask: ITask;
+  @Input() public pending = false;
+  @Output() public submit = new EventEmitter<ITaskCreationData>();
 
+  public form: FormGroup;
+
+  public constructor(private readonly formBuilder: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.form = this.formBuilder.group({
+      name: [
+        this.existingTask ? this.existingTask.name : '',
+        [Validators.required, Validators.maxLength(120)]
+      ],
+      description: [this.existingTask ? this.existingTask.description : '', Validators.required],
+      status: [this.existingTask ? this.existingTask.status : TaskStatus.New, Validators.required]
+    });
+  }
+
+  public submitForm(): void {
+    if (this.validateForm()) {
+      this.submit.emit(this.form.value);
+    }
+  }
+
+  private validateForm(): boolean {
+    Object.values(this.form.controls).forEach((formControl) => {
+      formControl.updateValueAndValidity();
+      formControl.markAsTouched();
+    });
+
+    return this.form.valid;
+  }
 }
